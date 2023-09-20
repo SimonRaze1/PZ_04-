e107c6e06b7549ff1f88127df1f68210f89032de
shirokov.dd2226 <shirokov.dd2226@COM409-L1>
2023-09-20 15:07:15 +0300

    Добавьте файлы проекта.

---
+++ PZ_04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    namespace PZ_04
+    {
+        internal class Program
+        {
+            static void Main(string[] args) 
+            {
+                int i;
+                for (i = 0; i <= 15; i++)
+                {
+                    Console.WriteLine(" " + i);
+                } 
+                Console.ReadLine();
+            }
+        }
+    }
